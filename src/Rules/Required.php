1eb700870b0b1c74bd6846c78655a64c0d94b82c
xanish <danishali7866@gmail.com>
2021-08-17 19:44:22 +0530

    Added required validation rule

---
+++ src/Rules/Required.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oshomo\CsvUtils\Rules;
+
+use Oshomo\CsvUtils\Contracts\ValidationRuleInterface;
+
+class Required implements ValidationRuleInterface
+{
+    /**
+     * Determines if the validation rule passes. This is where we do the
+     * actual validation. If the validation passes return true else false.
+     *
+     * @param mixed $value
+     * @param array $parameters
+     * @param array $row
+     * @return bool
+     */
+    public function passes($value, array $parameters, array $row): bool
+    {
+        return null !== $value && '' !== $value;
+    }
+
+    /**
+     * Get the validation error message. Specify the message that should
+     * be returned if the validation fails. You can make use of the
+     * :attribute and :value placeholders in the message string.
+     */
+    public function message(): string
+    {
+        return 'The :attribute value is required on line :line.';
+    }
+}
