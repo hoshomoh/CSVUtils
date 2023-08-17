a16db12d67ac3be59b64831a65bdf0a7a18badd7
Tomas Benedikt <tomas.benedikt@gmail.com>
2023-08-17 11:26:30 +0200

    fix: add default value
    Add default value to rules argument in Validator::__construct to suppress php deprecation warning

--- src/Validator/Validator.php
+++ src/Validator/Validator.php
@@ -114,7 +114,7 @@ class Validator
     /**
      * Create a new Validator instance.
      */
-    public function __construct(string $filePath, string $delimiter = ',', array $rules, array $messages = [])
+    public function __construct(string $filePath, string $delimiter = ',', array $rules = [], array $messages = [])
     {
         $this->filePath = $filePath;
         $this->delimiter = $delimiter;
